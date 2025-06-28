fd594e9915510c34f25aface53a4262471ecad8c
sorena nemati <sorenanemati.dev@gmail.com>
2025-06-29 00:41:47 +0330

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,12 +1,12 @@
-let fourthSect = document.getElementsByClassName("fourth-section");
-let shadowUp = document.getElementsByClassName("shadow-up");
-let seeMoreBtn = document.getElementsByClassName("see-more");
+let fourthSect = document.querySelector(".fourth-section");
+let shadowUp = document.querySelector(".shadow-up");
+let seeMoreBtn = document.querySelector(".see-more");
 
 // Events
-seeMoreBtn.addEventListener("click", seeMore())
+seeMoreBtn.addEventListener("click", seeMore);
 
 // Functions
 function seeMore () {
-    fourthSect.style.height = "auto !important";
-    shadowUp.style.display = "none !important";
+    fourthSect.style.height = "auto";
+    shadowUp.style.display = "none";
 }
