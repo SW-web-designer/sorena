e6db359c1a3857920ade55ea4a992b3e5b106254
sorena nemati <sorenanemati.dev@gmail.com>
2025-06-30 00:46:43 +0330

    Update home.js

--- script/home.js
+++ script/home.js
@@ -3,10 +3,20 @@ let shadowUp = document.querySelector(".shadow-up");
 let seeMoreBtn = document.querySelector(".see-more");
 
 // Events
-seeMoreBtn.addEventListener("click", seeMore);
+seeMoreBtn.addEventListener("click", run1and2);
 
 // Functions
+// 1
 function seeMore () {
     fourthSect.style.height = "105.15rem";
+    shadowUp.style.opacity = "0";
+}
+// 2
+function dNone () {
     shadowUp.style.display = "none";
+}
+// 3
+function run1and2 () {
+    seeMore();
+    setTimeout(dNone, 500);
 }
